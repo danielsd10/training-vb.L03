934fcd3a6651851a7b068bc4e93413ff97e6563f
Daniel Salas <dsalasd@gmd.com.pe>
2018-01-12 17:35:21 -0500

    Clase Contacto

---
+++ LibretaConsola/Contacto.vb
@@ -0,0 +1,11 @@
+﻿Public Class Contacto
+
+    Public Property Nombre As String
+    Public Property Apellido As String
+    Public Property DNI As String
+    Public Property FechaNacimiento As Date
+    Public Property Direccion As String
+    Public Property Telefono As String
+    Public Property Correo As String
+
+End Class
